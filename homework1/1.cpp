fc80a9b64b7d81a30ec95f4427fc8bfcf58a5860
Denis Volkov <37876353+ElvenVillage@users.noreply.github.com>
2021-11-09 17:39:53 +0000

    Update 1.cpp

--- homework1/1.cpp
+++ homework1/1.cpp
@@ -9,7 +9,7 @@ int main()
 
 	int x, y;
 	
-	int lx, rx, ly, ry;
+	int lx = 0, rx = 0, ly = 0, ry = 0;
 	double _rcos, _lcos;
 	_lcos = 1; _rcos = 1;
 
@@ -22,14 +22,15 @@ int main()
 		
 		
 		double cos = (x0*x+y0*y)/(sqrt(x*x+y*y)*sqrt(x0*x0+y0*y0));
+		cos = std::round(cos * 10000000000.0) / 10000000000.0;
         
-		if (cos < _lcos && (y0*x < x0*y)) 
+		if (cos <= _lcos && (y0*x < x0*y)) 
 		{
 			_lcos = cos;
 			lx = x; ly = y;
 
 		}
-		if (cos < _rcos && (y0*x >= x0*y)) 
+		if (cos <= _rcos && (y0*x >= x0*y)) 
 		{
 			_rcos = cos;
 			rx = x; ry = y;
