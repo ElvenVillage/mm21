9161ff08c2d9783ff97448a8a4fe6a33b4852791
Denis Volkov <37876353+ElvenVillage@users.noreply.github.com>
2021-11-12 12:45:50 +0000

    Update 2.cpp

--- homework2/2.cpp
+++ homework2/2.cpp
@@ -1,14 +1,63 @@
 #include <iostream>
 #include <fstream>
+#include <utility>
+#include <vector>
+#include <cmath>
 
+int main(int argc, char** argv) {
 
-int main()
-{
-	std::ifstream infile("in.txt");
-	int a, b;
+    if (argc == 2) {
+        std::ifstream infile(argv[1]);
 
-	while (infile >> a >> b)
-	{
-		std::cout << a;
-	}
+        std::vector < std::pair < double, double >> obstacles;
+
+        double h;
+        double x = 0;
+        infile >> h;
+
+        double vx, vy;
+        infile >> vx >> vy;
+
+        double g = -9.81;
+
+        int interval = 0;
+        int size = 0;
+
+        while (true) {
+            int target = (vx > 0) ? interval : interval - 1;
+            if (target < 0) {
+                std::cout << "0" << std::endl;
+                break;
+            }
+            if (target > size - 1) {
+                if (infile && infile.peek() == EOF) {
+                    std::cout << size << std::endl;
+                } else {
+                    double x_obs, h_obs;
+                    infile >> x_obs >> h_obs;
+                    std::pair < double, double > temp;
+                    temp.first = x_obs;
+                    temp.second = h_obs;
+                    obstacles.push_back(temp);
+                    size++;
+                }
+            }
+
+            double t = std::abs(x - obstacles[target].first) / vx;
+
+            vy = vy + g * t;
+            x = x + vx * t;
+            h = h + vy * t + 0.5 * g * t * t;
+
+            if (h <= 0) {
+                std::cout << target << std::endl;
+                break;
+            }
+            if (h > obstacles[target].second) {
+                (vx > 0) ? interval++ : interval--;
+            } else {
+                vx = vx * (-1);
+            }
+        }
+    } else {}
 }
